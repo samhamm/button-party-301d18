00280ad3349004bd84f816e61759adb45f6618d1
Sam Hamm <samhamm@gmail.com>
2017-03-30 15:50:18 -0700

    after the break

--- public/app.js
+++ public/app.js
@@ -19,7 +19,6 @@ function setPhrase(){
 // Don't forget the part where we display the code
 
 // Page JS action can be set up here; DO THIS LAST, DAMMIT
-// page('route/params', setColor, setRadius, setPhrase)
 
 //Event listener; build this AFTER the 3 top functions
 // $(form).on('change', callback...)
